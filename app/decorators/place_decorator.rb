b4ee795a8022c0a642d1869b3d9dfb54f81f2fe0
Ar2s <arti114@o2.pl>
2015-08-29 14:42:44 +0000

    Add decorator for avg_price in places

--- app/decorators/place_decorator.rb
+++ app/decorators/place_decorator.rb
@@ -4,4 +4,12 @@ class PlaceDecorator < Draper::Decorator
   def full_address
     "#{street}, #{city}, #{country}"
   end
+  
+  def averag_rating_show
+    "Average rating: "
+    
+  end
+  def avg_price_show
+    "Average price: #{avg_price}"
+  end
 end
