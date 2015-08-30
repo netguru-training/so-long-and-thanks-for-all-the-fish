f8ee856449a24f33961158a5955fc317f3324a3a
Ar2s <arti114@o2.pl>
2015-08-30 12:17:47 +0000

    Add views and CRUD for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,36 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   expose(:events) { Event.all.includes(:place) }
-  expose_decorated(:event)
+  expose_decorated(:event, attributes: :event_params)
+
+  def create
+    if event.save
+      redirect_to event_path(event), notice: I18n.t('shared.created', resource: 'Event')
+    else
+      render 'events/new'
+    end
+  end
+
+  def update
+    if event.save
+      redirect_to event_path(event), notice: I18n.t('shared.updated', resource: 'Event')
+    else
+      render 'events/edit'
+    end
+  end
+
+  def destroy
+    if event.destroy
+      redirect_to events_path, notice: I18n.t('shared.deleted', resource: 'Event')
+    else
+      render 'events/show'
+    end
+  end
+  
+  private
+
+    def event_params
+      params.require(:event).permit(:title, :description, :date, :place_id)
+    end
+
 end
