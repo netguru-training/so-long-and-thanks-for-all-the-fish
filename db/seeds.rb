e660e2cd06132a6dc7e07e63b99ee5e420821b6d
Ar2s <arti114@o2.pl>
2015-08-30 10:44:53 +0000

    Add real places to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,50 @@ puts 'CREATED ADMIN USER: ' << user.email
 
 User.create!(email: 'admin@admin.com', password: 'adminadmin')
 
-3.times do
   Place.create!(
-    name: Faker::Company.name,
-    street: Faker::Address.street_name,
-    city: Faker::Address.city,
-    country: Faker::Address.country,
-    avg_price: '5.0',
-    description: 'Restaurant'
-
+    name: '3kolory',
+    street: 'Wiankowa 3',
+    city: 'Poznań',
+    country: 'PL',
+    avg_price: '7.0',
+    description: 'Cafe grill & restaurant'
   )
-end
+    Place.create!(
+    name: 'Blue Marina',
+    street: 'Chojnicka 49',
+    city: 'Poznań',
+    country: 'PL',
+    avg_price: '20.0',
+    description: 'Restaurant'
+  )
+    Place.create!(
+    name: 'Naleśnikarnia Gramofon - Stary Rynek',
+    street: 'Wrocławska 2',
+    city: 'Poznań',
+    country: 'PL',
+    avg_price: '17.0',
+    description: 'Restaurant'
+  )
+    Place.create!(
+    name: 'Jugoslavia',
+    street: 'Ratajczaka 18',
+    city: 'Poznań',
+    country: 'PL',
+    avg_price: '12.0',
+    description: 'Restaurant'
+  )
+    Place.create!(
+    name: 'Zielona Weranda Cafe',
+    street: 'Paderewskiego 7',
+    city: 'Poznań',
+    country: 'PL',
+    avg_price: '35.0',
+    description: 'Cafe grill & restaurant'
+  )
 
-1.times do
-  Review.create!(text: Faker::Lorem.sentence(10), grade: rand(1..6), place_id: 1, user_id: 1)
+
+for i in 1..5 do
+  Review.create!(text: Faker::Lorem.sentence(10), grade: rand(1..6), place_id: i, user_id: 1)
 end
 
 5.times do
@@ -31,6 +61,6 @@ end
     title: Faker::Company.name,
     description: Faker::Lorem.sentence(10),
     date: Faker::Date.between(2.days.ago, Date.today),
-    place_id: rand(1..3)
+    place_id: rand(1..5)
   )
 end
